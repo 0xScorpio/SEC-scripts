f461c7e55e0512494cf41b0c24da662ef0070aba
0xScorpio <140411254+0xScorpio@users.noreply.github.com>
2024-01-29 17:02:49 +0200

    Update SEC1.ps1
    Added PNE, PLS, override update and logging in Comments attribute

--- SEC1.ps1
+++ SEC1.ps1
@@ -1,6 +1,5 @@
-﻿## [SEC1] TAGS on description - Computer inactivity and last logon ##
+## [SEC1] TAGS on description - Computers/Users inactivity, last logon ##
 
-Write-Host ""
 Write-Host @"
    _____  
   / ____| 
@@ -10,75 +9,90 @@ Write-Host @"
  |_____/ 
 
 __________________________________________________________________________
-[SEC1] tags all USER or COMPUTER objects, based on number of days inactive
+[SEC1] TAGS all user or computer objects, based on number of days inactive
 __________________________________________________________________________
 
 "@ -ForegroundColor Cyan
 
-
 ############## VARIABLES TO CHANGE ################
-$OUPath = ""
+$OUPath = "OU-PATH"
 $dayThreshold = 70
+# This is for converting Greek time signatures to AM or PM
+[System.Threading.Thread]::CurrentThread.CurrentCulture = 'en-US'
 ###################################################
 
 $currentDate = Get-Date
 
+Write-Host "[SEC1] is about to apply tags unto the following OU path with the current threshold:"
+Write-Host ""
+Write-Host "$OUPath" -ForegroundColor Yellow
+Write-Host ""
+Write-Host "Threshold: $dayThreshold days" -ForegroundColor Red
+Write-Host ""
 $userChoice = Read-Host "Tagging means updating the description attribute. What object type are we tagging? (user/computer)"
 
 if ($userChoice -eq "computer") {
     # Tag Computer objects with [SEC1] depending on dayThreshold variable
-    $comps = Get-ADComputer -Filter * -SearchBase $OUPath -SearchScope Subtree -Properties SamAccountName, LastLogonDate, LastLogon, Description
+    $comps = Get-ADComputer -Filter * -SearchBase $OUPath -SearchScope Subtree -Properties SamAccountName, LastLogonDate, Description, Comment, PasswordLastSet, PasswordNeverExpires
 
     foreach ($comp in $comps) {
         $inactivedays = ($currentDate - $comp.LastLogonDate).Days
-        $lastlog = [datetime]::FromFileTime($comp.LastLogon).ToString('dd/MM/yyyy [HH:mm]')
+        $lastlog = $comp.LastLogonDate.ToString('dd/MM/yyyy [HH:mm tt]')
 
         $existingDescription = $comp.Description
+        $existingComment = $comp.Comment
+        $pls = $comp.PasswordLastSet
+        $pne = $comp.PasswordNeverExpires
 
         if ($inactivedays -ge $dayThreshold) {
-            $newDescription = "[SEC1] Inactive: $inactivedays days. LastLogon: $lastlog Edited: $currentDate"
+            $newDescription = "[SEC1] Edited: $currentDate INACTIVE: $inactivedays days. LastLogon: $lastlog PwdLastSet: $pls PwdNeverExpires: $pne"
 
-            # If an existing description exists, either print or append the new information
-            if ($existingDescription) {
-                Write-Host ""
-                Write-Host "EXISTING DESCRIPTION EXISTS FOR" $comp.SamAccountName
-                Write-Host ""
-                ## Uncomment this if you don't want to overwrite the current description
-                $newDescription = "$newDescription | $existingDescription"
+            # Check if [SEC1] already exists in the description and override it
+            if ($existingDescription -like "*[SEC1]*") {
+                $existingDescription = $existingDescription -replace "\[SEC1\].*", $newDescription
             }
 
-        Set-ADComputer -Identity $comp.SamAccountName -Description $newDescription
-        Write-Host "Updated description for" $comp.SamAccountName
+            # Update the Description attribute
+            Set-ADComputer -Identity $comp.SamAccountName -Description $newDescription
 
+            # Append the new description to the Comment attribute
+            $newComment = "$newDescription | $existingComment"
+            Set-ADComputer -Identity $comp.SamAccountName -Replace @{Comment=$newComment}
+
+            Write-Host "Updated description and appended comment for $($comp.SamAccountName)"
         }
     }
 } elseif ($userChoice -eq "user") {
     # Tag User objects with [SEC1] depending on dayThreshold variable
 
     # Pull all users within a search scope
-    $users = Get-ADUser -Filter * -SearchBase $OUPath -SearchScope Subtree -Properties SamAccountName, LastLogonDate, LastLogon, Description
+    $users = Get-ADUser -Filter * -SearchBase $OUPath -SearchScope Subtree -Properties SamAccountName, LastLogonDate, Description, Comment, PasswordLastSet, PasswordNeverExpires
 
     foreach ($user in $users) {
         $inactivedaysU = ($currentDate - $user.LastLogonDate).Days
-        $lastLogU = [datetime]::FromFileTime($user.LastLogon).ToString('dd/MM/yyy [HH:mm]')
+        $lastlogU = $user.LastLogonDate.ToString('dd/MM/yyyy [HH:mm tt]')
 
         $existingDescriptionU = $user.Description
+        $existingCommentU = $user.Comment
+        $plsu = $user.PasswordLastSet
+        $pneu = $user.PasswordNeverExpires
 
         if ($inactivedaysU -ge $dayThreshold) {
-            $newDescriptionU = "[SEC1] Inactive: $inactivedaysU days. LastLogon: $lastlogU Edited: $currentDate"
+            $newDescriptionU = "[SEC1] Edited: $currentDate INACTIVE: $inactivedaysU days. LastLogon: $lastlogU PwdLastSet: $plsu PwdNeverExpires: $pneu"
 
-            # If an existing description exists, either print or append the new information
-            if ($existingDescriptionU) {
-                Write-Host ""
-                Write-Host "EXISTING DESCRIPTION EXISTS FOR" $user.SamAccountName
-                Write-Host ""
-                $newDescriptionU = "$newDescriptionU | $existingDescriptionU"
+            # Check if [SEC1] already exists in the description and override it
+            if ($existingDescriptionU -like "*[SEC1]*") {
+                $existingDescriptionU = $existingDescriptionU -replace "\[SEC1\].*", $newDescriptionU
             }
 
+            # Update the Description attribute
             Set-ADUser -Identity $user.SamAccountName -Description $newDescriptionU
-            Write-Host "Updated description for" $user.SamAccountName
+
+            # Append the new description to the Comment attribute
+            $newCommentU = "$newDescriptionU | $existingCommentU"
+            Set-ADUser -Identity $user.SamAccountName -Replace @{Comment=$newCommentU}
+
+            Write-Host "Updated description and appended comment for $($user.SamAccountName)"
         }
     }
 }
-
-
