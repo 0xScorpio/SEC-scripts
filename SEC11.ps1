785400ade053d21c8d8631abe2843809e3808939
0xScorpio <140411254+0xScorpio@users.noreply.github.com>
2024-01-31 16:23:17 +0200

    Added error-handling
    try/catch

--- SEC11.ps1
+++ SEC11.ps1
@@ -1,4 +1,4 @@
-﻿Write-Host ""
+Write-Host ""
 Write-Host @"
    _____  
   / ____| 
@@ -19,68 +19,80 @@ _______________________________________________
 function Get-SMBVersions {
     $smbRegistryPath = "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters"
 
-    $smb1Enabled = (Get-ItemProperty -Path $smbRegistryPath).SMB1
-    $smb2Enabled = (Get-ItemProperty -Path $smbRegistryPath).SMB2
+    try {
+        $smb1Enabled = (Get-ItemProperty -Path $smbRegistryPath -ErrorAction Stop).SMB1
+        $smb2Enabled = (Get-ItemProperty -Path $smbRegistryPath -ErrorAction Stop).SMB2
 
-    Write-Host "================================"
-    Write-Host "          SMB Versions          "
-    Write-Host "================================"
-    Write-Host "  SMB1: $(If ($smb1Enabled -eq 1) {'Enabled'})"
-    Write-Host "  SMB2: $(If ($smb2Enabled -eq 1) {'Enabled'})"
-    Write-Host "________________________________"
-    Write-Host ""
-    Write-Host ""
+        Write-Host "================================"
+        Write-Host "          SMB Versions          "
+        Write-Host "================================"
+        Write-Host "  SMB1: $(If ($smb1Enabled -eq 1) {'Enabled'})"
+        Write-Host "  SMB2: $(If ($smb2Enabled -eq 1) {'Enabled'})"
+        Write-Host "________________________________"
+        Write-Host ""
+        Write-Host ""
+    } catch {
+        Write-Host "Error accessing registry path: $smbRegistryPath"
+    }
 }
 
 # Function to check TLS values
 function Get-TLSValues {
     $tlsRegistryPath = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols"
 
-    $ssl20EnabledClient = (Get-ItemProperty -Path "$tlsRegistryPath\SSL 2.0\Client").Enabled
-    $ssl20EnabledServer = (Get-ItemProperty -Path "$tlsRegistryPath\SSL 2.0\Server").Enabled
-    $ssl30EnabledClient = (Get-ItemProperty -Path "$tlsRegistryPath\SSL 3.0\Client").Enabled
-    $ssl30EnabledServer = (Get-ItemProperty -Path "$tlsRegistryPath\SSL 3.0\Server").Enabled
-    $tls10EnabledClient = (Get-ItemProperty -Path "$tlsRegistryPath\TLS 1.0\Client").Enabled
-    $tls10EnabledServer = (Get-ItemProperty -Path "$tlsRegistryPath\TLS 1.0\Server").Enabled
-    $tls11EnabledClient = (Get-ItemProperty -Path "$tlsRegistryPath\TLS 1.1\Client").Enabled
-    $tls11EnabledServer = (Get-ItemProperty -Path "$tlsRegistryPath\TLS 1.1\Server").Enabled
-    $tls12EnabledClient = (Get-ItemProperty -Path "$tlsRegistryPath\TLS 1.2\Client").Enabled
-    $tls12EnabledServer = (Get-ItemProperty -Path "$tlsRegistryPath\TLS 1.2\Server").Enabled
+    try {
+        $ssl20EnabledClient = (Get-ItemProperty -Path "$tlsRegistryPath\SSL 2.0\Client" -ErrorAction Stop).Enabled
+        $ssl20EnabledServer = (Get-ItemProperty -Path "$tlsRegistryPath\SSL 2.0\Server" -ErrorAction Stop).Enabled
+        $ssl30EnabledClient = (Get-ItemProperty -Path "$tlsRegistryPath\SSL 3.0\Client" -ErrorAction Stop).Enabled
+        $ssl30EnabledServer = (Get-ItemProperty -Path "$tlsRegistryPath\SSL 3.0\Server" -ErrorAction Stop).Enabled
+        $tls10EnabledClient = (Get-ItemProperty -Path "$tlsRegistryPath\TLS 1.0\Client" -ErrorAction Stop).Enabled
+        $tls10EnabledServer = (Get-ItemProperty -Path "$tlsRegistryPath\TLS 1.0\Server" -ErrorAction Stop).Enabled
+        $tls11EnabledClient = (Get-ItemProperty -Path "$tlsRegistryPath\TLS 1.1\Client" -ErrorAction Stop).Enabled
+        $tls11EnabledServer = (Get-ItemProperty -Path "$tlsRegistryPath\TLS 1.1\Server" -ErrorAction Stop).Enabled
+        $tls12EnabledClient = (Get-ItemProperty -Path "$tlsRegistryPath\TLS 1.2\Client" -ErrorAction Stop).Enabled
+        $tls12EnabledServer = (Get-ItemProperty -Path "$tlsRegistryPath\TLS 1.2\Server" -ErrorAction Stop).Enabled
 
-    Write-Host "================================"
-    Write-Host "          TLS Versions          "
-    Write-Host "================================"
-    Write-Host "  Client SSL 2.0: $(If ($ssl20EnabledClient -eq 4294967295) {'Enabled'})"
-    Write-Host "  Server SSL 2.0: $(If ($ssl20EnabledServer -eq 4294967295) {'Enabled'})"
-    Write-Host "  Client SSL 3.0: $(If ($ssl30EnabledClient -eq 4294967295) {'Enabled'})"
-    Write-Host "  Server SSL 3.0: $(If ($ssl30EnabledServer -eq 4294967295) {'Enabled'})"
-    Write-Host "  Client TLS 1.0: $(If ($tls10EnabledClient -eq 4294967295) {'Enabled'})"
-    Write-Host "  Server TLS 1.0: $(If ($tls10EnabledServer -eq 4294967295) {'Enabled'})"
-    Write-Host "  Client TLS 1.1: $(If ($tls11EnabledClient -eq 4294967295) {'Enabled'})"
-    Write-Host "  Server TLS 1.1: $(If ($tls11EnabledServer -eq 4294967295) {'Enabled'})"
-    Write-Host "  Client TLS 1.2: $(If ($tls12EnabledClient -eq 4294967295) {'Enabled'})"
-    Write-Host "  Server TLS 1.2: $(If ($tls12EnabledServer -eq 4294967295) {'Enabled'})"
-    Write-Host "________________________________"
+        Write-Host "================================"
+        Write-Host "          TLS Versions          "
+        Write-Host "================================"
+        Write-Host "  Client SSL 2.0: $(If ($ssl20EnabledClient -eq 4294967295) {'Enabled'})"
+        Write-Host "  Server SSL 2.0: $(If ($ssl20EnabledServer -eq 4294967295) {'Enabled'})"
+        Write-Host "  Client SSL 3.0: $(If ($ssl30EnabledClient -eq 4294967295) {'Enabled'})"
+        Write-Host "  Server SSL 3.0: $(If ($ssl30EnabledServer -eq 4294967295) {'Enabled'})"
+        Write-Host "  Client TLS 1.0: $(If ($tls10EnabledClient -eq 4294967295) {'Enabled'})"
+        Write-Host "  Server TLS 1.0: $(If ($tls10EnabledServer -eq 4294967295) {'Enabled'})"
+        Write-Host "  Client TLS 1.1: $(If ($tls11EnabledClient -eq 4294967295) {'Enabled'})"
+        Write-Host "  Server TLS 1.1: $(If ($tls11EnabledServer -eq 4294967295) {'Enabled'})"
+        Write-Host "  Client TLS 1.2: $(If ($tls12EnabledClient -eq 4294967295) {'Enabled'})"
+        Write-Host "  Server TLS 1.2: $(If ($tls12EnabledServer -eq 4294967295) {'Enabled'})"
+        Write-Host "________________________________"
+    } catch {
+        Write-Host "Error accessing registry path: $tlsRegistryPath"
+    }
 }
 
 function Get-Hashes {
     $hashesPath = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Hashes"
 
-    $md5Check = (Get-ItemProperty -Path "$hashesPath\MD5").Enabled
-    $shaCheck = (Get-ItemProperty -Path "$hashesPath\SHA").Enabled
-    $sha256Check = (Get-ItemProperty -Path "$hashesPath\SHA256").Enabled
-    $sha384Check = (Get-ItemProperty -Path "$hashesPath\SHA384").Enabled
-    $sha512Check = (Get-ItemProperty -Path "$hashesPath\SHA512").Enabled
+    try {
+        $md5Check = (Get-ItemProperty -Path "$hashesPath\MD5" -ErrorAction Stop).Enabled
+        $shaCheck = (Get-ItemProperty -Path "$hashesPath\SHA" -ErrorAction Stop).Enabled
+        $sha256Check = (Get-ItemProperty -Path "$hashesPath\SHA256" -ErrorAction Stop).Enabled
+        $sha384Check = (Get-ItemProperty -Path "$hashesPath\SHA384" -ErrorAction Stop).Enabled
+        $sha512Check = (Get-ItemProperty -Path "$hashesPath\SHA512" -ErrorAction Stop).Enabled
 
-    Write-Host "================================"
-    Write-Host "             Hashes             "
-    Write-Host "================================"
-    Write-Host "   MD5: $(If ($md5Check -eq 4294967295) {'Enabled'})"
-    Write-Host "   SHA: $(If ($shaCheck -eq 4294967295) {'Enabled'})"
-    Write-Host "   SHA256: $(If ($sha256Check -eq 4294967295) {'Enabled'})"
-    Write-Host "   SHA384: $(If ($sha384Check -eq 4294967295) {'Enabled'})"
-    Write-Host "   SHA512: $(If ($sha512Check -eq 4294967295) {'Enabled'})"
-    Write-Host "________________________________"
+        Write-Host "================================"
+        Write-Host "             Hashes             "
+        Write-Host "================================"
+        Write-Host "   MD5: $(If ($md5Check -eq 4294967295) {'Enabled'})"
+        Write-Host "   SHA: $(If ($shaCheck -eq 4294967295) {'Enabled'})"
+        Write-Host "   SHA256: $(If ($sha256Check -eq 4294967295) {'Enabled'})"
+        Write-Host "   SHA384: $(If ($sha384Check -eq 4294967295) {'Enabled'})"
+        Write-Host "   SHA512: $(If ($sha512Check -eq 4294967295) {'Enabled'})"
+        Write-Host "________________________________"
+    } catch {
+        Write-Host "Error accessing registry path: $hashesPath"
+    }
 }
 
 # Check SMB versions
