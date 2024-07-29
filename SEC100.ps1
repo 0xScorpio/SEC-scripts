55a895809e85e3a5e29901f5364a855654555e42
0xScorpio <140411254+0xScorpio@users.noreply.github.com>
2024-07-29 13:38:44 +0300

    Update SEC100.ps1

--- SEC100.ps1
+++ SEC100.ps1
@@ -1803,27 +1803,31 @@ function Init-SEC15 {
 }
 
 function Init-SEC16 {
-   # Define the username for which you want to identify the lockout source
-   $lockedOutUser = Read-Host "Enter username to check"
+# Define the username for which you want to identify the lockout source
+$lockedOutUser = Read-Host "Enter username to check"
 
-   # Search the Security event log for lockout events related to the specified user
-   $events = Get-WinEvent -FilterHashtable @{
-       LogName = 'Security'
-       ID = 4740  # Event ID for account lockouts
-   } -ErrorAction SilentlyContinue | Where-Object { $_.Message -like "*$lockedOutUser*" }
+# Search the Security event log for lockout events related to the specified user
+$filterXml = @"
+<QueryList>
+  <Query Id="0" Path="Security">
+    <Select Path="Security">*[System[(EventID=4740)]] and *[EventData[Data[@Name='TargetUserName'] and (Data='$lockedOutUser')]]</Select>
+  </Query>
+</QueryList>
+"@
 
-   if ($events) {
-       Write-Host "Lockout events for user '$lockedOutUser':"
-       $events | ForEach-Object {
-           $time = $_.TimeCreated
-           $message = $_.Message
-           Write-Host "Event Time: $time"
-           Write-Host "Message: $message"
-           Write-Host "------------------------------------------"
-       }
-   } else {
-       Write-Host "No lockout events found for user '$lockedOutUser'."
-   }
+$events = Get-WinEvent -FilterXml $filterXml -ErrorAction SilentlyContinue
+
+if ($events) {
+    Write-Host "Lockout events for user '$lockedOutUser':" -ForegroundColor Green
+    $events | ForEach-Object {
+        $time = $_.TimeCreated
+        $message = $_.Message
+        Write-Host "Event Time: $time" -ForegroundColor Yellow
+        Write-Host "Message: $message" -ForegroundColor Yellow
+        Write-Host "------------------------------------------" -ForegroundColor Gray
+    }
+} else {
+    Write-Host "No lockout events found for user '$lockedOutUser'." -ForegroundColor Red
 }
 
 function Init-SEC17 {
